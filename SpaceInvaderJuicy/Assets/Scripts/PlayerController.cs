69ab41460b8c6a471e6c2b2c43fa76bfa97e8dfe
Nolan <nolan.jutard@gmail.com>
2022-01-11 10:12:31 +0100

    Better player movement

--- SpaceInvaderJuicy/Assets/Scripts/PlayerController.cs
+++ SpaceInvaderJuicy/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 50f;
+    private float speed = 0.25f;
 
     [SerializeField]
     private Transform muzzle;
@@ -27,39 +27,99 @@ public class PlayerController : MonoBehaviour
     public float maxDistanceUp;
     public float maxDistanceDown;
 
+    private Rigidbody2D rb2D;
+    float moveHz=0;
+    float moveVrt=0;
+    float acceleration = 1;
+    float MinMovement = 0.1f;
+
+    private ParticleSystem bubble;
+
+    public bool betterMovement;
     // Start is called before the first frame update
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
         col = GetComponent<BoxCollider2D>();
         startPos = transform.position;
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        if (!betterMovement)
         {
-            transform.Translate(speed * Time.deltaTime, 0, 0);
-        }
-        else if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Translate(-speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.Z))
-        {
-            transform.Translate(0, speed * Time.deltaTime * 0.7f, 0);
-            if(transform.position.y > startPos.y + maxDistanceUp)
+            if (Input.GetKey(KeyCode.Z))
             {
-                transform.position = new Vector2(transform.position.x, startPos.y + maxDistanceUp);
+                transform.Translate(0, speed * Time.deltaTime * 0.7f, 0);
+                if (transform.position.y > startPos.y + maxDistanceUp)
+                {
+                    transform.position = new Vector2(transform.position.x, startPos.y + maxDistanceUp);
+                }
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(0, -speed * Time.deltaTime * 0.7f, 0);
+                if (transform.position.y < startPos.y - maxDistanceDown)
+                {
+                    transform.position = new Vector2(transform.position.x, startPos.y - maxDistanceDown);
+                }
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+            else if (Input.GetKey(KeyCode.Q))
+            {
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
             }
         }
-        else if (Input.GetKey(KeyCode.S))
+        else
         {
-            transform.Translate(0, -speed * Time.deltaTime*0.7f, 0);
-            if (transform.position.y < startPos.y - maxDistanceDown)
+            if (rb2D)
             {
-                transform.position = new Vector2(transform.position.x, startPos.y - maxDistanceDown);
+                if (Input.GetAxis("Horizontal") != 0)
+                {
+                    //moveHz += acceleration * Input.GetAxis("Horizontal");
+                    moveHz = Input.GetAxis("Horizontal") * 2;
+                    if (moveHz > 2)
+                    {
+                        moveHz = 2;
+                    }
+                    else if (moveHz < -2)
+                    {
+                        moveHz = -2;
+                    }
+                }
+                else
+                {
+                    moveHz /= (2 * (1 + Time.deltaTime));
+                    if (moveHz < MinMovement)
+                        moveHz = 0;
+                }
+
+                if (Input.GetAxis("Vertical") != 0)
+                {
+                    moveVrt = Input.GetAxis("Vertical") * 2;
+                    if (moveVrt > 2)
+                    {
+                        moveVrt = 2;
+                    }
+                    else if (moveVrt < -2)
+                    {
+                        moveVrt = -2;
+                    }
+                }
+                else
+                {
+                    moveVrt /= (2 * (1 + Time.deltaTime));
+                    if (moveVrt < MinMovement)
+                        moveVrt = 0;
+                }
+
+                rb2D.velocity = new Vector2(moveHz * speed, moveVrt * speed);
             }
         }
         if (currentBullet == null && Input.GetKey(KeyCode.Space))
@@ -68,11 +128,15 @@ public class PlayerController : MonoBehaviour
             currentBullet = instantiateBullet;
         }
     }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
-        GameManager.Instance.UpdateLives();
-        StopAllCoroutines();
-        StartCoroutine(Respawn());
+        if (other.gameObject.tag == "Bullet" || other.gameObject.name == "BulletSpawner(Clone)")
+        {
+            GameManager.Instance.UpdateLives();
+            StopAllCoroutines();
+            StartCoroutine(Respawn());
+        }
     }
 
     System.Collections.IEnumerator Respawn()
