80663e19c2181763202bee84de9f35628c7c00a0
Nolan <nolan.jutard@gmail.com>
2022-01-12 10:03:51 +0100

    Particle system (Bubble) + modif script

--- SpaceInvaderJuicy/Assets/Scripts/PlayerController.cs
+++ SpaceInvaderJuicy/Assets/Scripts/PlayerController.cs
@@ -23,20 +23,19 @@ public class PlayerController : MonoBehaviour
 
     public float maxDistanceUp;
     public float maxDistanceDown;
+    public float maxRadiusTurn;
 
-    private Rigidbody2D rb2D;
-    [SerializeField]
     float speed;
-    [SerializeField]
     float speedV;
     [SerializeField]
     public float maxSpeed;
     [SerializeField]
     public float secondToDecelerate;
-    float moveHz = 0;
-    float moveVrt = 0;
     [SerializeField]
     float secondToMaxSpeed;
+    float moveHz = 0;
+    float moveVrt = 0;
+
     float MinMovement = 0.1f;
 
     private ParticleSystem bubble;
@@ -48,7 +47,6 @@ public class PlayerController : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>();
         col = GetComponent<BoxCollider2D>();
         startPos = transform.position;
-        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -59,18 +57,12 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKey(KeyCode.Z))
             {
                 transform.Translate(0, speed * Time.deltaTime * 0.7f, 0);
-                if (transform.position.y > startPos.y + maxDistanceUp)
-                {
-                    transform.position = new Vector2(transform.position.x, startPos.y + maxDistanceUp);
-                }
+
             }
             else if (Input.GetKey(KeyCode.S))
             {
                 transform.Translate(0, -speed * Time.deltaTime * 0.7f, 0);
-                if (transform.position.y < startPos.y - maxDistanceDown)
-                {
-                    transform.position = new Vector2(transform.position.x, startPos.y - maxDistanceDown);
-                }
+
             }
 
             if (Input.GetKey(KeyCode.D))
@@ -84,118 +76,99 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            if (rb2D)
+            moveHz = Input.GetAxis("Horizontal");
+            moveVrt = Input.GetAxis("Vertical");
+
+            #region Acceleration
+            if (Input.GetKey(KeyCode.D))
             {
-                moveHz = Input.GetAxis("Horizontal");
-                moveVrt = Input.GetAxis("Vertical");
-
-                //if (moveHz != 0 || moveVrt != 0)
-                //{
-                //    if (speed < maxSpeed)
-                //    {
-                //        speed += acceleration * Time.deltaTime;
-                //    }
-                //    if (speed > maxSpeed)
-                //    {
-                //        speed = maxSpeed;
-                //    }
-                //}
-                //else if (moveHz == 0 && moveVrt == 0)
-                //{
-                //    if (speed > 0)
-                //    {
-                //        speed -= decelerate * Time.deltaTime;
-                //    }
-                //    if (speed < 0)
-                //    {
-                //        speed = 0;
-                //    }
-                //}
-                //rb2D.velocity = new Vector2(moveHz * speed, moveVrt * speed);
-
-                #region Acceleration
-                if (Input.GetKey(KeyCode.D))
+                if (speed < maxSpeed)
                 {
-                    if (speed < maxSpeed)
-                    {
-                        speed += (maxSpeed/secondToMaxSpeed) * Time.deltaTime;
-                    }
-                    if (speed > maxSpeed)
-                    {
-                        speed = maxSpeed;
-                    }
+                    speed += (maxSpeed / secondToMaxSpeed) * Time.deltaTime;
                 }
-                if (Input.GetKey(KeyCode.A))
+                if (speed > maxSpeed)
                 {
-                    if (speed > -maxSpeed)
-                    {
-                        speed -= (maxSpeed / secondToMaxSpeed) * Time.deltaTime;
-                    }
-                    if (speed < -maxSpeed)
-                    {
-                        speed = -maxSpeed;
-                    }
+                    speed = maxSpeed;
                 }
-                
-                if (Input.GetKey(KeyCode.W))
-                {
-                    if (speedV < maxSpeed)
-                    {
-                        speedV += (maxSpeed / secondToMaxSpeed) * Time.deltaTime;
-                    }
-                    if (speedV > maxSpeed)
-                    {
-                        speedV = maxSpeed;
-                    }
-                }
-                if (Input.GetKey(KeyCode.S))
-                {
-                    if (speedV > -maxSpeed)
-                    {
-                        speedV -= (maxSpeed / secondToMaxSpeed) * Time.deltaTime;
-                    }
-                    if (speedV < -maxSpeed)
-                    {
-                        speedV = -maxSpeed;
-                    }
-                }
-                #endregion
-                #region Decelerate
-                if (moveHz == 0 )
-                {
-                    if (speed < 0)
-                    {
-                        speed += (maxSpeed/secondToDecelerate) * Time.deltaTime;
-                    }
-                    else if (speed > 0)
-                    {
-                        speed -= (maxSpeed / secondToDecelerate) * Time.deltaTime;
-                    }
-                    else
-                    {
-                        speed = 0;
-                    }
-                }
-                if (moveVrt == 0 )
-                {
-                    if (speedV < 0)
-                    {
-                        speedV += (maxSpeed / secondToDecelerate) * Time.deltaTime;
-                    }
-                    else if (speedV > 0)
-                    {
-                        speedV -= (maxSpeed / secondToDecelerate) * Time.deltaTime;
-                    }
-                    else
-                    {
-                        speedV = 0;
-                    }
-                }
-                #endregion
-                transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y + speedV * Time.deltaTime);
-
             }
+            if (Input.GetKey(KeyCode.A))
+            {
+                if (speed > -maxSpeed)
+                {
+                    speed -= (maxSpeed / secondToMaxSpeed) * Time.deltaTime;
+                }
+                if (speed < -maxSpeed)
+                {
+                    speed = -maxSpeed;
+                }
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                if (speedV < maxSpeed)
+                {
+                    speedV += (maxSpeed / secondToMaxSpeed) * Time.deltaTime;
+                }
+                if (speedV > maxSpeed)
+                {
+                    speedV = maxSpeed;
+                }
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                if (speedV > -maxSpeed)
+                {
+                    speedV -= (maxSpeed / secondToMaxSpeed) * Time.deltaTime;
+                }
+                if (speedV < -maxSpeed)
+                {
+                    speedV = -maxSpeed;
+                }
+            }
+            #endregion
+            #region Decelerate
+            if (moveHz == 0)
+            {
+                if (speed < 0)
+                {
+                    speed += (maxSpeed / secondToDecelerate) * Time.deltaTime;
+                }
+                else if (speed > 0)
+                {
+                    speed -= (maxSpeed / secondToDecelerate) * Time.deltaTime;
+                }
+                else
+                {
+                    speed = 0;
+                }
+            }
+            if (moveVrt == 0)
+            {
+                if (speedV < 0)
+                {
+                    speedV += (maxSpeed / secondToDecelerate) * Time.deltaTime;
+                }
+                else if (speedV > 0)
+                {
+                    speedV -= (maxSpeed / secondToDecelerate) * Time.deltaTime;
+                }
+                else
+                {
+                    speedV = 0;
+                }
+            }
+            #endregion
+            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y + speedV * Time.deltaTime);
         }
+        if (transform.position.y > startPos.y + maxDistanceUp)
+        {
+            transform.position = new Vector2(transform.position.x, startPos.y + maxDistanceUp);
+        }
+        if (transform.position.y < startPos.y - maxDistanceDown)
+        {
+            transform.position = new Vector2(transform.position.x, startPos.y - maxDistanceDown);
+        }
+
         if (currentBullet == null && Input.GetKey(KeyCode.Space))
         {
             GameObject instantiateBullet = Instantiate(bulletPrefab, muzzle.position, Quaternion.identity);
